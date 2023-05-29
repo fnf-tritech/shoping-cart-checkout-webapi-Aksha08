77855f70cb0fe67c50f26a3e91fdb7f7ce9c72cc
Aksha08 <129684044+Aksha08@users.noreply.github.com>
2023-05-29 09:48:30 -0700

    Add files via upload

---
+++ CheckoutServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebApplication3.tests
+{
+    public class CheckoutServiceTests
+    {
+        [Fact]
+        public void CalculateTotalPrice_ReturnsCorrectTotalPrice()
+        {
+            // Arrange
+            var checkoutService = new CheckoutService();
+            string skus = "AAABBBCCDD";
+
+
+
+            // Act
+            decimal totalPrice = checkoutService.CalculateTotalPrice(skus);
+
+
+
+            // Assert
+            decimal expectedTotalPrice = 260;  // Total price for the given skus
+            Assert.Equal(expectedTotalPrice, totalPrice);
+        }
+
+    }
+}
