82ab33e23592abf52383346415000634df120338
Aksha08 <129684044+Aksha08@users.noreply.github.com>
2023-05-29 23:04:32 +0530

    Add files via upload

---
+++ WebApplications3.tests/WebApplications3.tests/CheckoutServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication3.Controllers;
+
+namespace WebApplications3.tests
+{
+    public class CheckoutServiceTest
+    {
+        [Fact]
+        public void CalculateTotalPrice_EmptySKUs_ReturnsZero()
+        {
+            // Arrange
+            var checkoutService = new CheckoutService();
+
+            // Act
+            decimal totalPrice = checkoutService.CalculateTotalPrice("");
+
+            // Assert
+            Assert.Equal(0, totalPrice);
+        }
+        [Fact]
+        public void CalculateTotalPrice_SingleSKU_ReturnsCorrectPrice()
+        {
+            // Arrange
+            var checkoutService = new CheckoutService();
+
+            // Act
+            decimal totalPrice = checkoutService.CalculateTotalPrice("A");
+
+            // Assert
+            Assert.Equal(50, totalPrice);
+        }
+        [Fact]
+        public void CalculateTotalPrice_MultipleSKUs_ReturnsCorrectPrice()
+        {
+            // Arrange
+            var checkoutService = new CheckoutService();
+
+            // Act
+            decimal totalPrice = checkoutService.CalculateTotalPrice("AB");
+
+            // Assert
+            Assert.Equal(80, totalPrice);
+        }
+    }
+}
---
+++ WebApplications3.tests/WebApplications3.tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebApplications3.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
